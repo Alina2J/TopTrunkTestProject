06f49b24c36118b3a88f441a6bbbae7eff096c68
Alina Lebedeva <104134739+Alina2J@users.noreply.github.com>
2024-08-31 07:54:30 +0300

    First_Main_Commit

---
+++ js/slider.js
@@ -0,0 +1,14 @@
+$('.single-item').slick({
+    infinite: true,
+    slidesToShow: 2,
+    dots: true,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+});
